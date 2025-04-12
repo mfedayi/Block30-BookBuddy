d6f8c5c24651b8160b6709bf1df6597ba8eb1468
Sonia Gupta <soniagupta@Sonias-Air.attlocal.net>
2025-04-12 15:50:01 -0400

    Added functionality to Books.jsx

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,3 +1,44 @@
-const Books = () => {};
+import { useGetBooksQuery } from "./bookSlice";
+import { useNavigate } from "react-router-dom";
 
-export default Books;
+
+export default function Books({ setSelectedPuppyId }) {
+    const { data: allBooks, error, status, isLoading } = useGetBooksQuery();
+    const navigate = useNavigate();
+    const viewBook = (id) => {
+      navigate(`/book/${id}`);
+    };
+    console.log(allBooks); // Take out later
+    if (isLoading) {
+      return <h1> Is loading </h1>;
+    }
+    if (error) {
+      return <h1>{error}</h1>;
+    }
+    return (
+    <>
+    <article>
+        <h2>Library Catalog</h2>
+            <ul>
+            {isLoading && <li>Loading books...</li>}
+              {allBooks.books.map((p) => (
+                <li key={p.id}>
+                  <h3>
+                    #{p.id} {p.title} by {p.author} 
+                  </h3>
+                  <figure>
+                    <img src={p.imageUrl} alt={p.name} />
+                  </figure>
+                  <button type = "button" onClick={() => navigate(`/books/${p.id}`)}>
+                    See details
+                  </button>
+                </li>
+                ))}
+            </ul> 
+          </article>
+    </>
+);
+}
+
+
+
